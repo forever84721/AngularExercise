abfd1c0d35ead8aafaf3dc145ccd4511d9ceb9e7
forever84721 <forever84721@yahoo.com.tw>
2019-04-27 12:46:08 +0800

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { LoginComponent } from './Page/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'Login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
